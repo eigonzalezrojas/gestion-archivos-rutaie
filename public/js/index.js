5c45df232d5e0f95ec99ec3deb9950bfeb90e640
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-12-17 11:53:58 -0300

    menu movil, footer

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-button').addEventListener('click', function() {
+    const navbar = document.getElementById('navbar-sticky');
+    navbar.classList.toggle('hidden');
+  });
