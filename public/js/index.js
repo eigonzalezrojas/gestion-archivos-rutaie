118535b7682757efe4b5c0d04e12b3b4b117ce28
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-12-17 20:43:50 -0300

    objetivo view

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
 document.getElementById('menu-button').addEventListener('click', function() {
     const navbar = document.getElementById('navbar-sticky');
     navbar.classList.toggle('hidden');
-  });
+});
