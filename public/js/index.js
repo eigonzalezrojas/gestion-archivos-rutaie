e68c2286da27a6300f049ddc53aa7b949d14479c
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-12-20 09:06:22 -0300

    successfully view nosotros

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,12 @@ document.getElementById('menu-button').addEventListener('click', function() {
     const navbar = document.getElementById('navbar-sticky');
     navbar.classList.toggle('hidden');
 });
+
+document.querySelectorAll('.barra-lateral a').forEach(el => {
+    el.addEventListener('mouseover', function() {
+        this.classList.add('text-blue-500'); // Cambiar color con TailwindCSS
+    });
+    el.addEventListener('mouseout', function() {
+        this.classList.remove('text-blue-500');
+    });
+});
